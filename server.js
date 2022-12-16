d155fcaebbddd073418055baa2344da6bab54105
Leandro Bezerra Santos <leandrodiasbezerra95@gmail.com>
2022-12-16 11:12:21 -0300

    get values of form on the  back-end

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 const express = require("express");
 const app = express();
+const body_parser = require('body-parser')
 
-app.set('view engine', 'ejs');//Dizendo para o express usar o EJS como view engine
+app.set('view engine', 'ejs')
 
-app.get('/:name', (request, response) => {
-    const name =  request.params;
+app.use(express.static('public'))
+app.use(express.json());
+app.use(body_parser.urlencoded({ extended:false }));
 
-    response.render('index', {
-        name
-    });  
+
+app.get('/', (request, response) => {
+    response.render('index')
+})
+
+app.get('/ask', (request, response) => {
+    response.render('ask')
+})
+
+app.post('/askSave', (request, response) =>{
+    const {title, description} = request.body;
+
+    response.status(200).json(`Titulo: ${title}, Descrição: ${description}`);
 })
 
 app.listen(2323, () => console.log('Server in running 👌'));
